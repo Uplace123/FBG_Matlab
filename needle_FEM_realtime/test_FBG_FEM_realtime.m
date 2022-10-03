61e8b9d3f42e52a4909ec59d0643de8179080e33
Uplace123 <98064986+Uplace123@users.noreply.github.com>
2022-10-02 23:50:42 -0400

    finished

--- needle_FEM_realtime/test_FBG_FEM_realtime.m
+++ needle_FEM_realtime/test_FBG_FEM_realtime.m
@@ -2,13 +2,17 @@
 clear;
 clc;
 
+% close all pnet connection
+
+
 addpath ../sm130_interrogator_matlab/
 addpath ../rawdata_process/
 addpath ./WYZ_FEM
-
+close_pnet();
 filename = fullfile(tempdir,'communicate.dat');
 a = exist(filename);
-[ds, ks, sb, h, l, PropertyTable] = FBG_integrated_FEM_Ogden_UTru([0;0;0;0]);
+curvature = zeros(4,2);
+[ds, ks, sb, h, l, PropertyTable] = FBG_integrated_FEM_Ogden_UTru(curvature(:,2));
 
 if a == 0
     fid = fopen(filename,'w');
@@ -17,6 +21,7 @@ if a == 0
     fwrite(fid,sb,'double');
     fwrite(fid,h,'double');
     fwrite(fid,l,'double');
+    fwrite(fid,curvature,'double');
     fclose(fid);
 end
 
@@ -26,6 +31,7 @@ sz_ks = size(ks);
 sz_sb = size(sb);
 sz_h = size(h);
 sz_l = size(l);
+sz_curvature = size(curvature);
 
 m = memmapfile(filename, 'Writable',true, 'Format', ...
     {'double', sz_ds, 'ds';
@@ -33,39 +39,40 @@ m = memmapfile(filename, 'Writable',true, 'Format', ...
     'double', sz_sb, 'sb';
     'double', sz_h, 'h';
     'double', sz_l, 'l';
+    'double', sz_curvature, 'curvature';
     });
 
 NumChannel = 2;
 NumAA = 4;
 RefData = [];
 RawData = [];
-curvature = zeros(4,2);
+
+
+% run ini_interrogator.m
+interrogator = ini_interrogator('IPaddress','192.168.1.11','Port',1852,'ReadTimeout',0.1);
 
 % read the reference data
-RefData = mean(Read_interrogator(10,2,4,'IPaddress','192.168.1.11','Port',1852,'ReadTimeout',0.1));
-
+RefData = mean(Read_interrogator(20,2,4,interrogator));
 
 %while 1
 while 1
-    %tic
+    tic
     
-    RawData = Read_interrogator(1,NumChannel,NumAA,'IPaddress','192.168.1.11','Port',1852,'ReadTimeout',0.1);
-    
+    RawData = Read_interrogator(1,NumChannel,NumAA,interrogator);
+    %toc about 0.01s
     curvature = data_process(RawData,RefData,NumChannel,NumAA);
-    
-    [ds, ks, sb, h, l, PropertyTable] = FBG_integrated_FEM_Ogden_UTru(curvature(:,1));
-    % ds 1*166 double
-    % ks 1*166 double
-    % sb 1*1
-    % h 1*1
-    % l 1*1
+    %toc about 0.01s
+    [ds, ks, sb, h, l, PropertyTable] = FBG_integrated_FEM_Ogden_UTru(curvature(:,2));
+    %toc about 0.09s
+
     % propertytable interval mut alphaT GammaT
     m.Data.ds(sz_ds(1),1:sz_ds(2)) = ds;
     m.Data.ks(sz_ks(1),1:sz_ks(2)) = ks;
     m.Data.sb(sz_sb(1),sz_sb(2)) = sb;
     m.Data.h(sz_h(1),sz_h(2)) = h;
     m.Data.l(sz_l(1),sz_h(2)) = l;
-    %toc % about 0.10s
+    m.Data.curvature(1:sz_curvature(1),1:sz_curvature(2)) = curvature;
+    toc % about 0.09s
 end
 
 
