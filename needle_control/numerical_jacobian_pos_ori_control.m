73d9815bc1f875e746f0b338c470d1a3930ac760
Uplace123 <yxu168@jhu.edu>
2022-10-11 14:51:31 -0400

    functionize needle control; write a test file to test realtime needle control

--- needle_control/numerical_jacobian_pos_ori_control.m
+++ needle_control/numerical_jacobian_pos_ori_control.m
@@ -1,15 +1,20 @@
-clear
-clc;
+function du = numerical_jacobian_pos_ori_control(xd,kp,ic,sb, l, ti, Nel, Mu, Alpha, Interval,NumChannel,NumAA,interrogator,RefData,AA_lcn)
 
-addpath ../needle_FEM_realtime/WYZ_FEM/ 
-%% FEM Parameters
-S.sb = 20;
-S.l = 40;
-S.ti = 25;
-S.Nel = 20;
-S.Mu = 3000;
-S.Alpha = 8;
-S.function = @FBG_integrated_FEM_Ogden_UTru;
+%% FEM and FBG Parameters
+S.sb = sb;
+S.l = l;
+S.ti = ti;
+S.Nel = Nel;
+S.Mu = Mu;
+S.Alpha = Alpha;
+S.function = @FBG_FEM_realtime;
+S.Interval = Interval;
+S.NumChannel = NumChannel;
+S.NumAA = NumAA; 
+S.interrogator = interrogator;
+S.RefData = RefData;
+S.AA_lcn = AA_lcn;
+
 %% Control and ODE Parameters
 % Requires needle tip position and orientation as feedback
 
@@ -20,55 +25,33 @@ S.function = @FBG_integrated_FEM_Ogden_UTru;
 % For actual control implementation with constant time steps, the control
 % is simply u = du*dt
 
-S.xd = [1; -0.1]; % desired tip position and orientation
-S.Kp = diag([2, 2]); % proportional gain -> too large can result in non-converging FEM
-tspan = [0, 1]; % simulation time span
-x0 = zeros(2, 1); % initial tip position and orientation
-u0 = zeros(2, 1); % initial base position and orientation
-ic = [x0; u0]; % initial conditions
+S.xd = xd; % desired tip position and orientation
+S.Kp = kp; % proportional gain -> too large can result in non-converging FEM
+%tspan = [0, 1]; % simulation time span
+% x0 = zeros(2, 1); % initial tip position and orientation
+% u0 = zeros(2, 1); % initial base position and orientation
+% ic = [x0; u0]; % initial conditions
 
 %% Simulation and Plotting
-[t, xu] = ode45(@(t, xu) sys_ode(t, xu, S), tspan, ic);
+%[t, xu] = ode45(@(t, xu) sys_ode(t, xu, S), tspan, ic);
 
+%% get realtime control outputs
 
-% % Plot outputs
-% f1 = figure;
-% plot(t, xu(:, 1), t, xu(:, 2));
-% legend('Tip Pos', 'Tip Ori')
-% xlabel('Time (s)')
-% title('Needle Tip States')
-% % Plot base maneuvers
-% f2 = figure;
-% plot(t, xu(:, 3), t, xu(:, 4));
-% legend('Base Pos', 'Base Ori')
-% xlabel('Time (s)')
-% title('Needle Base Maneuvers')
-% % Plot needle shapes
-% f3 = figure;
-% m = linspace(-S.sb, S.l, S.Nel + 1);
-% plt = plot(m, rand(1, S.Nel + 1), 'k-', 'LineWidth', 2);
-% xlabel('x (mm)');
-% ylabel('y (mm)');
-% title('Needle Shape')
-% for i = 1:length(t)
-% [ds, ~] = S.function(S.sb, S.l, xu(i, 3), xu(i, 4), S.ti, S.Nel, S.Mu, S.Alpha);
-% axis equal
-% grid on
-% set(plt, 'YData', ds);
-% drawnow;
-% pause(0.2);
-% end
+% output
+dxudt = sys_ode(ic,S);
+
 %% Auxiliary Functions
 % System ODE
-function dxudt = sys_ode(t, xu, S)
+function du = sys_ode(xu, S)
 x = xu(1:2);
 u = xu(3:4);
 du = sys_control(x, u, S); % du is the control inputs, but dependent on u, which is the previous needle base configuration
-[ds, ks] = S.function(S.sb, S.l, du(1), du(2), S.ti, S.Nel, S.Mu, S.Alpha);
-dx1 = ds(end);
-dx2 = ks(end);
-dx = [dx1; dx2];
-dxudt = [dx; du];
+% no need to get dx
+% [ds, ks] = S.function(S.sb, S.l, du(1), du(2), S.ti, S.Nel, S.Mu, S.Alpha,S.Interval,S.NumChannel,S.NumAA,S.interrogator,S.RefData,S.AA_lcn);
+% dx1 = ds(end);
+% dx2 = ks(end);
+% dx = [dx1; dx2];
+% dxudt = [dx; du];
 end
 
 % Control inputs based on feedback linearization and computed torque law
@@ -79,7 +62,7 @@ end
 
 % Numerical jacobian based on FEM simulation
 function y = input_output_fem(u, S)
-[ds, ks] = S.function(S.sb, S.l, u(1), u(2), S.ti, S.Nel, S.Mu, S.Alpha);
+[ds, ks] = S.function(S.sb, S.l, u(1), u(2), S.ti, S.Nel, S.Mu, S.Alpha,S.Interval,S.NumChannel,S.NumAA,S.interrogator,S.RefData,S.AA_lcn);
 y = zeros(2, 1);
 y(1) = ds(end);
 y(2) = ks(end);
---
+++ needle_control/test_FBG_FEM_control.m
@@ -0,0 +1,109 @@
+% test needle control which recieve real time FEM feedback
+
+clear;
+clc;
+
+%% Add path
+% for interrogator reading
+addpath ../sm130_interrogator_matlab/
+% for FEM
+addpath ../needle_FEM_realtime/WYZ_FEM/invChol/
+addpath ../needle_FEM_realtime/WYZ_FEM/
+addpath ../needle_FEM_realtime/ 
+% for needle control
+
+% for motor control
+
+
+%% Close all pnet connection
+close_pnet();
+
+%% FEM needed input
+sb = 20;
+l = 145;
+Db = 0; % control input 1
+Kb = 0; % control input 2
+ti = 25;
+Nel = sb + l; % 1mm elements
+Mu = [0, 5e3];
+Alpha = [2, 3];
+Interval = {[-sb, 0], [0, l + 10]};
+needle_length = 165; % the 18G FBG needle length
+AA_lcn_base = [65, 100, 135]; % measured from base, skipping the AA near tip due to poor readings
+AA_lcn_tip = needle_length - AA_lcn_base; % measured from tip
+AA_lcn = sb + l - AA_lcn_tip; % measured from sb
+% save params for plotting use
+save("plot_params.mat",'sb','l','Db','Kb','ti','Nel','Mu','Alpha','Interval','AA_lcn_base');
+
+%% FBG information
+NumChannel = 2;
+NumAA = 4; % this should keep tha same as needle
+
+%% control and ode parameters
+xd = [1; -0.1]; % desired tip position and orientation
+Kp = diag([2, 2]); % proportional gain -> too large can result in non-converging FEM
+x0 = zeros(2, 1); % initial tip position and orientation
+u0 = zeros(2, 1); % initial base position and orientation
+ic = [x0; u0]; % initial conditions
+
+
+
+%% Initialization, data and memory
+curvatures = zeros(NumAA,2); %num_AA * 2
+curvatures_xy = curvatures(:,1);
+curvatures_xz = curvatures(:,2);
+[ds, ks, xs] = FBG_integrated_FEM_Ogden_UTru(sb, l, Db, Kb, ti, Nel, Mu, ...
+                                             Alpha, Interval, curvatures_xz, AA_lcn);
+
+filename = fullfile(tempdir,'communicate.dat');
+a = exist(filename, 'file');
+if a == 0
+    %plot needed parameters
+    fid = fopen(filename,'w');
+    fwrite(fid,ds,'double');
+    fwrite(fid,ks,'double');
+    fwrite(fid,xs,'double');
+    fwrite(fid,curvatures,'double');
+    fclose(fid);
+end
+
+% get output size
+sz_ds = size(ds);
+sz_ks = size(ks);
+sz_xs = size(xs);
+sz_curvatures = size(curvatures);
+
+m = memmapfile(filename, 'Writable',true, 'Format', ...
+    {'double', sz_ds, 'ds';
+    'double', sz_ks, 'ks';
+    'double', sz_xs, 'xs';
+    'double', sz_curvatures, 'curvature';
+    });
+
+%% Read interrogator data for FBG initialization
+RefData = [];
+% run ini_interrogator.m
+interrogator = ini_interrogator('IPaddress','192.168.1.11','Port',1852,'ReadTimeout',0.1);
+% read the reference data
+RefData = mean(Read_interrogator(20,2,NumAA,interrogator));
+
+%% Main loop
+%while 1
+
+% test one time
+for i = 1:1:1
+    % get du for next step
+    du = numerical_jacobian_pos_ori_control(xd,kp,ic,sb, l, ti, Nel, Mu, Alpha,...
+                                                Interval,NumChannel,NumAA,interrogator,...
+                                                RefData,AA_lcn);
+      % change xd ic for next step
+      
+      % motor control
+
+%     % story data for plotting
+%     m.Data.ds(sz_ds(1), 1:sz_ds(2)) = ds;
+%     m.Data.ks(sz_ks(1), 1:sz_ks(2)) = ks;
+%     m.Data.xs(sz_xs(1), 1:sz_xs(2)) = xs;
+%     m.Data.curvature(1:sz_curvatures(1),1:sz_curvatures(2)) = curvatures;
+
+end
